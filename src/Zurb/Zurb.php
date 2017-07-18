d0bf270a8b09bba0d15210e9e3f2bd5474d7217c
Miguel Piedrafita <soy@miguelpiedrafita.com>
2017-07-18 12:35:38 +0200

    Fix stubs

--- src/Zurb/Zurb.php
+++ src/Zurb/Zurb.php
@@ -53,8 +53,8 @@ class Zurb extends Preset
             resource_path('assets/sass/initial-variables.sass')
         );
 
-        copy(__DIR__.'/foundation-stubs/_settings.scss', resource_path('assets/sass/_settings.scss'));
-        copy(__DIR__.'/foundation-stubs/app.scss', resource_path('assets/sass/app.scss'));
+        copy(__DIR__.'/zurb-stubs/_settings.scss', resource_path('assets/sass/_settings.scss'));
+        copy(__DIR__.'/zurb-stubs/app.scss', resource_path('assets/sass/app.scss'));
     }
 
     /**
@@ -68,6 +68,6 @@ class Zurb extends Preset
             resource_path('assets/js/bootstrap.js')
         );
 
-        copy(__DIR__.'/foundation-stubs/bootstrap.js', resource_path('assets/js/bootstrap.js'));
+        copy(__DIR__.'/zurb-stubs/bootstrap.js', resource_path('assets/js/bootstrap.js'));
     }
 }
